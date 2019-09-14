22e58419abf9f692560876651b4407dc6bf99eaf
faustoandrade <ayf1207@gmail.com>
2019-09-14 11:44:14 -0500

    generated todo item controller, form and todo item partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :todo_lists do
     resources :todo_items
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "todo_lists#index" # creando ruta directa al programa
 end
