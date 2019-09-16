d73d5ab8a24a238414554d06c4363148a75f6a9f
faustoandrade <ayf1207@gmail.com>
2019-09-16 13:45:16 -0500

    added styles and views in index, edit, new, show.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     resources :todo_items do
       member do
         patch :complete
-        end
       end
     end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "todo_lists#index" # creando ruta directa al programa
 end
