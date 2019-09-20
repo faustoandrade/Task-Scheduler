c46b5626b282d7e4887c5be2cadff39ab62b5321
faustoandrade <ayf1207@gmail.com>
2019-09-20 08:03:52 -0500

    added validations and styles

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,9 @@
 class TodoList < ApplicationRecord
-  has_many :todo_items # una lista tiene muchos items 
+  has_many :todo_items # una lista tiene muchos items
+
+  # make validations for the creation of a task
+  validates :title, :description, presence: true
+  validates :title, length: { minimum: 4 }
+  validates :description, length: { maximum: 500 }
+  validates :title, uniqueness: true
 end
